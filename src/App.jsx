aad3a9fc563b7103f64902edd8fdc718abcfeb3f
alicubimet-gif <ali.cubimet@gmail.com>
2026-09-14 17:52:15 +0530

    style: load search storage and photo UI refinements

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import './styles/responsive.css';
 import './styles/crud-system.css';
 import './styles/settings-system.css';
 import './styles/saas-account.css';
+import './styles/ui-refinements.css';
 
 export function App() {
   return (
